559e4e0f7e4b78c8d067e651c7018e875be7e81e
Senzen18 <sendhanaruljothi@gmail.com>
2025-03-11 12:40:09 +0530

    single attention head

--- gpt.py
+++ gpt.py
@@ -5,10 +5,12 @@ from torch.optim import AdamW
 
 block_size = 8
 batch_size = 32
-max_iter = 3000
+max_iter = 5000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_interval = 300
+eval_interval = 500
 eval_iter = 200
+n_embed = 32
+learning_rate = 1e-3
 
 
 text = ''
@@ -16,6 +18,7 @@ with open('input.txt','r',encoding='utf-8') as f:
     text = f.read()
 
 vocab = sorted(list(set(text)))
+vocab_size = len(vocab)
 stoi = {s:i for i,s in enumerate(vocab)}
 itos = {i:s for i,s in enumerate(vocab)}
 encode = lambda text: [stoi[s] for s in text]
@@ -32,13 +35,38 @@ def get_batch(split):
     xb = torch.stack([train_data[i:i+block_size] for i in ix])
     yb = torch.stack([train_data[i+1:i+block_size+1] for i in ix])
     return xb.to(device),yb.to(device)
-
-class BigramLanguageModel(nn.Module):
-    def __init__(self,vocab_size):
+class Head(nn.Module):
+    def __init__(self,head_size):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size,vocab_size)
+        self.key = nn.Linear(n_embed,head_size,bias=False)
+        self.query = nn.Linear(n_embed,head_size,bias=False)
+        self.value = nn.Linear(n_embed,head_size,bias=False)
+        self.register_buffer('tril',torch.tril(torch.ones((block_size,block_size))))
+    def forward(self,x):
+        B,T,C = x.shape
+        q = self.query(x)
+        k = self.key(x)
+        v = self.value(x)
+
+        wei = q @ k.transpose(-2,-1) * C**-0.5
+        wei = wei.masked_fill(self.tril[:T,:T]==0,float('-inf'))
+        wei = F.softmax(wei,dim=-1)
+        out = wei @ v
+        return out
+class BigramLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size,n_embed)
+        self.position_embedding_table = nn.Embedding(block_size,n_embed)
+        self.lm_head = nn.Linear(n_embed,vocab_size)
+        self.sa_head = Head(n_embed)
     def forward(self, idx,target=None):
-        logits = self.token_embedding_table(idx)
+        B,T = idx.shape
+        tok_embed = self.token_embedding_table(idx)
+        pos_embed = self.position_embedding_table(torch.arange(T,device=device))
+        x = tok_embed + pos_embed
+        x = self.sa_head(x)
+        logits = self.lm_head(x)
         if target == None:
             loss = None
         else:
@@ -49,7 +77,8 @@ class BigramLanguageModel(nn.Module):
         return logits, loss
     def generate(self,idx,max_new_tokens):
         for _ in range(max_new_tokens):
-            logits,loss = self(idx)
+            idx_cond = idx[:,-block_size:]
+            logits,loss = self(idx_cond)
             logits = logits[:,-1,:]
             probs = F.softmax(logits,dim=1)
             idx_next = torch.multinomial(probs,num_samples=1)
@@ -73,9 +102,9 @@ def estimate_loss():
 
     
 
-model = BigramLanguageModel(len(vocab))
+model = BigramLanguageModel()
 model = model.to(device)
-optim = AdamW(model.parameters())
+optim = AdamW(model.parameters(),lr=learning_rate)
 batch_size = 32
 for step in range(max_iter):
 
